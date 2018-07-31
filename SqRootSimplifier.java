48005eb95aa8793ea1cf1b6d4954dc193b63a504
Sofia-Ortega <sofia.j.ortega101@gmail.com>
2018-07-31 14:38:03 -0500

    Changed array, trying to keep track of number of times factor appears

--- SqRootSimplifier.java
+++ SqRootSimplifier.java
@@ -3,7 +3,7 @@
 
 public class SqRootSimplifier {
   public static final Scanner CONSOLE = new Scanner(System.in);
-  //====================================DIVIDERFINDER==============================
+  //====================================DIVIDERFINDER (method)==============================
   public static int dividerFinder(int userNum){
     int divider = 1;
     int returnDivider = 1;
@@ -27,20 +27,8 @@ public class SqRootSimplifier {
     
   }
   
-//====================================FACTOR FINDER==========================
-/*
-public static void factoring(int x){
-    int y;
-  
-    for(int i=x; i >1; i = i -1){
-   y = x % i;
-      if ( y == 0){
-       System.out.println(i);
-      }
-   }
- }
-my factoring program. needs to be updated with variables but is short
-*/
+//====================================FACTOR FINDER (method)==========================
+
   public static int factorFinder(int userNum){
     int divider = 1;
     int factor = 1;
@@ -63,12 +51,23 @@ my factoring program. needs to be updated with variables but is short
     
     
   }
-    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //-------------------------------------------------------------------------------
   //=======================================MAIN====================================
+  //------------------------------------------------------------------------------- 
   
   public static void main ( String [] args){
 
-    System.out.println("Prompt: ");
+    System.out.println("Enter Square Root Number : ");
     int userNum = CONSOLE.nextInt();
 
     
@@ -77,7 +76,7 @@ my factoring program. needs to be updated with variables but is short
     int factorResult2 = 10;
     int counter = 0;
     int[] dividerArray = new int[20];
-    int[] counterArray = new int[20];
+    int[] counterArray = new int[25]; 
     
     
    
@@ -104,10 +103,52 @@ my factoring program. needs to be updated with variables but is short
     
     //gets last factor
     dividerArray[counter] = factorResult2;
+    int switchNumber = 0;
     
+    
+    for( int i = 0; i <= counter; i++ ) {
+      System.out.println("dividerArray[" + i + "]: " + dividerArray[i]);
+    }
+    //2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+    switch ( switchNumber ) {
+      case 2: 
+        counterArray[0] = counterArray[0] + 1;
+      case 3:
+      case 5:
+      case 7:
+      case 11:
+      case 13:
+      case 17:
+      case 19:
+      case 23:
+      case 29:
+      case 31:
+      case 37:
+      case 43:
+      case 47:
+      case 53:
+      case 59:
+      case 61:
+      case 67:
+      case 71:
+      case 73:
+      case 79:
+      case 83:
+      case 89:
+      case 97:
+        
+
+      
+    }
+    
+    /*
     int counter2 = 0; //new counter declared
     int occurence = 0;
     
+    //---------------------------------------------------------------------------
+    
+    
+    //Finding out number of occurences for each factor
     for(int i = 0; i <= counter; i++) {
       counter2 = 0;
       occurence = 0;
@@ -117,7 +158,7 @@ my factoring program. needs to be updated with variables but is short
       for(int j = 0; j <= counter; j++) {
         System.out.print(dividerArray[j] + " ");
         
-        //IMPORTANT IN counterArray:     Tens place = factor, ones place = times of occurence
+        //IMPORTANT: in counterArray:     Tens place = factor, ones place = times of occurence
         if(dividerArray[i] == dividerArray[j]) { 
           counterArray[counter2] = (dividerArray[i] * 10) + (occurence + 1);
           occurence = occurence + 1;
@@ -129,6 +170,7 @@ my factoring program. needs to be updated with variables but is short
       System.out.println("CounterArray: " + counterArray[counter2] + "\n");
     }
     
+    */
     
     
     
