87b1510449fe546dbc91187e142cac8d2f0ec64f
Sofia-Ortega <sofia.j.ortega101@gmail.com>
2018-08-04 14:26:03 -0500

    starting cplx num converter

---
+++ ComplexNumbersConverter.java
@@ -0,0 +1,8 @@
+public class ComplexNumbersConverter{
+  public static void main ( String args [] ) {
+    System.out.println("Enter a and b (a + bi)");
+    int a = CONSOLE.nextInt();
+    
+    
+  }
+}
